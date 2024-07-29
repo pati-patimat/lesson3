13d97f144f5fb90423e556afdb8b1911aa951476
pati-patimat <patya.khaib@yandex.ru>
2024-07-29 20:59:46 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Патимат'
+age = 25
+new_age = age + 5
+is_student = bool('true')
+print('Name:', name)
+print('Age:', age)
+print('New age:', new_age)
+print('Is student:', bool(is_student))
